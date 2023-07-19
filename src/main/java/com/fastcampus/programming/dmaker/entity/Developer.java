6f4a2e839aca227b93a74e948dcf6b07ba41f91d
kichul <kichul@learncha.com>
2023-07-19 13:59:25 +0900

    developer entity 생성 및 H2 데이터베이스 연결

---
+++ src/main/java/com/fastcampus/programming/dmaker/entity/Developer.java
@@ -0,0 +1,43 @@
+package com.fastcampus.programming.dmaker.entity;
+
+import com.fastcampus.programming.dmaker.type.DeveloperLevel;
+import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Developer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperLevel developerLevel;
+
+    @Enumerated(EnumType.STRING)
+    private DeveloperSkillType developerSkillType;
+
+    private Integer experienceYears;
+    private String memberId;
+    private String name;
+    private Integer age;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
